9e965de8dab14d03bc366e452e5f52b9b16c0aee
Gabriel <gabrieljvgg@gmail.com>
2026-05-19 07:09:53 -0300

    .env.example implementado

--- cardapio_restaurante/settings.py
+++ cardapio_restaurante/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Substitua as linhas antigas por essas:
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
